906c7016f21edae496ebdb14342ba197f1470c74
Salim Oussayfi <box@slimou.net>
2021-11-20 10:03:57 +0100

    SpringBoot TicTacToe

---
+++ TicTacToe-CC/src/test/java/net/slimou/tictactoecc/TicTacToeCcApplicationTests.java
@@ -0,0 +1,13 @@
+package net.slimou.tictactoecc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeCcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
