e13c9b5ab0804b10ae7d2996afa64b54abfd7410
Salim Oussayfi <box@slimou.net>
2021-11-21 08:54:20 +0100

    further tests

--- TicTacToe-CC/src/test/java/net/slimou/tictactoecc/TicTacToeCcApplicationTests.java
+++ TicTacToe-CC/src/test/java/net/slimou/tictactoecc/TicTacToeCcApplicationTests.java
@@ -63,7 +63,7 @@ class TicTacToeCcApplicationTests {
 	}
 
 	@Test
-	void isMatchWon() {
+	void isMatchWonTrue() {
 		char[][] playground = this.playgroundService.generateNewPlayground();
 		Move m1 = this.moveService.generateNewMoveObject('x',0,0);
 		this.playgroundService.setMoveOnPlayground(m1, playground);
@@ -73,6 +73,19 @@ class TicTacToeCcApplicationTests {
 		this.playgroundService.setMoveOnPlayground(m3, playground);
 		boolean isMatchWon = this.validationService.isMatchWonByCurrentMove(m3, playground);
 		assertTrue(isMatchWon);
+	}
+
+	@Test
+	void isMatchWonFalse() {
+		char[][] playground = this.playgroundService.generateNewPlayground();
+		Move m1 = this.moveService.generateNewMoveObject('x',0,0);
+		this.playgroundService.setMoveOnPlayground(m1, playground);
+		Move m2 = this.moveService.generateNewMoveObject('x',1,1);
+		this.playgroundService.setMoveOnPlayground(m2, playground);
+		Move m3 = this.moveService.generateNewMoveObject('x',1,2);
+		this.playgroundService.setMoveOnPlayground(m3, playground);
+		boolean isMatchWon = this.validationService.isMatchWonByCurrentMove(m3, playground);
+		assertFalse(isMatchWon);
 	}
 
 }
